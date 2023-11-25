76941914be9fbb23a859105c36f480d5732425f0
Amish Motwani <124799100+amisha-motwani@users.noreply.github.com>
2023-11-26 00:28:00 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -5,7 +5,7 @@
 const mongoose = require('mongoose');   //to require mongoose
 const connect= async ()=>{
 
-    await mongoose.connect('mongodb://127.0.0.1:27017/olymics').then(() => {   //if we fullfill the promise
+    await mongoose.connect('mongodb+srv://amishamotwani143:YkAnVKzq3VU6oQXQ@cluster0.mdcl1f0.mongodb.net/?retryWrites=true&w=majority/olymics').then(() => {   //if we fullfill the promise
         console.log("Connection Successful")
     }).catch((e) => {    //if we could'nt fullfill the promise, here we are using a call back function
         console.log("No Connection")
