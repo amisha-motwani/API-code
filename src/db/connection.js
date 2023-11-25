9631216ebc94d0322331ebdef26a32c1ab4dcff9
Amish Motwani <124799100+amisha-motwani@users.noreply.github.com>
2023-11-26 00:48:10 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,10 +2,12 @@
 
 // Code1:
 
+
 const mongoose = require('mongoose');   //to require mongoose
+require("dotenv").config();
 const connect= async ()=>{
 
-    await mongoose.connect('mongodb+srv://amishamotwani143:YkAnVKzq3VU6oQXQ@cluster0.mdcl1f0.mongodb.net/?retryWrites=true&w=majority/olymics').then(() => {   //if we fullfill the promise
+    await mongoose.connect(`mongodb+srv://${process.env.REACT_APP_USERNAME}:${process.env.REACT_APP_PASSWORD}@cluster0.mdcl1f0.mongodb.net/?retryWrites=true&w=majority/olymics`).then(() => {   //if we fullfill the promise
         console.log("Connection Successful")
     }).catch((e) => {    //if we could'nt fullfill the promise, here we are using a call back function
         console.log("No Connection")
